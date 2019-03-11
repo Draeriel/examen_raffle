ffda37590ece3414c7eb572a79aad3645f3096e5
draeriel <danieljuanbarbosa@gmail.com>
2019-03-11 17:13:23 +0100

    Primer commit: Visualizar la zapatilla

---
+++ scripts/main.js
@@ -0,0 +1,134 @@
+var shoe = {
+    "model": "Sacai x Nike LDV Waffle",
+    "colour": "Varsity Blue/Del Sol/Varsity Red",
+    "code": "BV0073-400",
+    "avaliable": "07/03/19",
+    "price": "180$"
+};
+
+var raffles = {
+
+    "Antonia Milano": {
+        "logo": "https://www.soleretriever.com/wp-content/uploads/2018/04/AntoniaMilano.jpg",
+        "country": "Italy",
+        "purchase": "Online Raffle",
+        "collection": "Postage Available",
+        "Sizes": "4 to 12 US",
+        "Opens": "live",
+        "Closes": "06/03 @ 11AM CET",
+        "url": "https://www.antonia.it/164-shoes"
+    },
+
+    "END": {
+        "logo": "https://www.soleretriever.com/wp-content/uploads/2018/04/End.jpg",
+        "country": "UK",
+        "purchase": "Online Raffle",
+        "collection": "Postage Available",
+        "Sizes": "5 to 12 UK",
+        "Opens": "live",
+        "Closes": "07/03 @ 12AM GMT",
+        "url": "https://launches.endclothing.com/"
+    },
+
+    "Foot Patrol": {
+        "logo": "https://www.soleretriever.com/wp-content/uploads/2018/04/FootPatrol.png",
+        "country": "France",
+        "purchase": "Online Raffle",
+        "collection": "Collection Only",
+        "Sizes": "36.5 to 47.5 EU",
+        "Opens": "live",
+        "Closes": "04/02 @ 10AM CET",
+        "url": "https://www.footpatrol.com/customer-service/raffle-fr/"
+    },
+
+    "Holypop": {
+        "logo": "https://www.soleretriever.com/wp-content/uploads/2018/04/HolyPop.png",
+        "country": "Italy",
+        "purchase": "Online FCFS",
+        "collection": "Postage Available",
+        "Sizes": "TBC",
+        "Opens": "announced",
+        "Closes": "07/02 @ 12AM CET",
+        "url": "https://www.holypopstore.com/en/footwear"
+    },
+
+    "Offspring": {
+        "logo": "https://www.soleretriever.com/wp-content/uploads/2018/04/OffSpring.jpg",
+        "country": "UK",
+        "purchase": "Online Raffle",
+        "collection": "Collection Only",
+        "Sizes": "3.5 to 7 UK",
+        "Opens": "live",
+        "Closes": "closed",
+        "url": "https://www.offspring.co.uk/release-dates"
+    },
+
+    "SNS": {
+        "logo": "https://www.soleretriever.com/wp-content/uploads/2018/04/SNS.jpg",
+        "country": "Swe, UK, Ger, Fr",
+        "purchase": "Online Raffle",
+        "collection": "Post and Collect",
+        "Sizes": "4 to 13 US",
+        "Opens": "live",
+        "Closes": "06/03 @ 11AM CET",
+        "url": "https://www.sneakersnstuff.com/en/937/sns-raffles"
+    },
+
+    "Solebox": {
+        "logo": "https://www.soleretriever.com/wp-content/uploads/2018/04/SoleBox.jpg'git",
+        "country": "Germany",
+        "purchase": "In-Store/Online",
+        "collection": "Post and Collect",
+        "Sizes": "41 to 46 EU",
+        "Opens": "announced",
+        "Closes": "When sold out",
+        "url": "https://www.solebox.com/en/Footwear/"
+    },
+
+};
+
+//exports.sole = {
+//    "shoe": shoe,
+//    "raffles": raffles
+//};
+
+
+window.onload = function () {
+    displayShoeInfo();
+}
+
+function getShoe() {
+    return this.shoe;
+}
+
+function displayShoeInfo() {
+    let shoe = this.getShoe();
+
+    let container = document.createElement('div');
+    container.setAttribute('class', 'mainProductContainer');
+
+    let img = document.createElement('img');
+    img.src = 'https://bit.ly/2EPgoOi';
+    img.setAttribute('class', 'mainProductImage');
+
+    let div = document.createElement('div');
+    div.setAttribute('class', 'mainProductInfo');
+
+    let h1 = document.createElement('h1');
+    h1.innerHTML = shoe.model;
+
+    let h3 = document.createElement('h3');
+    h3.innerHTML = shoe.colour;
+
+    let extraInfo = document.createElement('h3');
+    extraInfo.innerHTML = `${shoe.code}|${shoe.avaliable}|${shoe.price}`;
+
+    div.appendChild(h1);
+    div.appendChild(h3);
+    div.appendChild(extraInfo);
+
+    container.appendChild(img);
+    container.appendChild(div);
+    
+    document.body.appendChild(container);
+}
